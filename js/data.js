e38e252502d5c448d43818b55a804f86a5b51532
Grumstukas <tartenaite.toma@gmail.com>
2019-06-13 13:18:37 +0300

    links animations

--- js/data.js
+++ js/data.js
@@ -1,1 +1,46 @@
 "use strict";
+var Icons = [
+    {
+        icon: 'facebook',
+        link: '#'
+    },
+    {
+        icon: 'twitter',
+        link: '#'
+    },
+    {
+        icon: 'dribbble',
+        link: '#'
+    },
+    {
+        icon: 'pinterest',
+        link: '#'
+    },
+    {
+        icon: 'instagram',
+        link: '#'
+    }
+];
+
+var footerIcons = [
+    {
+        icon: 'facebook',
+        link: '#'
+    },
+    {
+        icon: 'twitter',
+        link: '#'
+    },
+    {
+        icon: 'dribbble',
+        link: '#'
+    },
+    {
+        icon: 'pinterest',
+        link: '#'
+    },
+    {
+        icon: 'instagram',
+        link: '#'
+    }
+];
