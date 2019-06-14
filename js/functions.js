de776d984fc3482f7ddac0d00cdee6cd05001633
Grumstukas <tartenaite.toma@gmail.com>
2019-06-14 16:34:31 +0300

    header knows in which section it is

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,62 @@
 "use strict";
+// general
+function elementHeight( path ) {
+    var height = parseFloat(window.getComputedStyle( document.querySelector( path ) ).height);
+    return height;
+}
+//parseFloat -                          ištraukia skaitinę aukščio vertę;
+//window.getComputedStyle( X ).height - ištraukia tekstinę stiliaus elemento (X) aukščio reikšmę;
+//document.querySelector( path ) -      nuoroda į konkretų HTML elementą
+
+function headerScrollDetector() {
+    var sections = [],
+        scroll = window.scrollY + elementHeight('header'),                      //duoda scroll aukštį esamu momentu.
+        headerLinkCount = document.querySelectorAll('header nav > a').length,   //suskaičiuoja konkretaus HTML elemento brolių kiekį
+        top = document.getElementById('testimonials').offsetTop,                //duoda HTML elemento conteinerio viršaus aukštį.
+        sectionID ='',                                                          //ciklas sukaups visus ID
+        sekcijuSarasas = [],                                                    //ciklas sukaups visus sekcijų pavadinimus su #
+        sekcijosPavadinimas = [],                                               //ciklas sukaups visus sekcijų pavadinimus be #
+        sectionNameNow = '';                                                    //sekcijos vardas kuri yra ekrano viršuje vardas
+        
+
+    for ( var i=0; i<headerLinkCount; i++ ) {                                             //ciklą riboja konkretaus HTML elemento brolių kiekis
+        sectionID = document.querySelectorAll('header nav > a')[i].getAttribute('href');  //ciklas iš konkrečių HTML brolių ims po vieną ir ištrauks nurodyto atributo vertę
+        if ( sectionID === '#' ) {                  //jeigu vertė tik #
+            sectionID = sectionID.substring(1);     //atima iš sekcijos ID #
+            sekcijosPavadinimas.push(sectionID);    //surenka sekcijų pavadinimus į vieną sąrašą;
+            sections.push(0);                       //saraše "sections" pirmu nariu įrašys - 0 
+            continue;                                // ir tikrins toliau.
+        }
+        top = document.querySelector(sectionID).offsetTop;  //gavęs atsakymą iš ifo, pasakys jo aukščio vertę, 
+        sections.push(top);                                 // ir įstums į sąrašą.
+        sekcijuSarasas.push(sectionID);                     //duoda visų sekcijų sąrašą su # (ar reikalingas ???)
+        sectionID = sectionID.substring(1);                 //atima iš sekcijos ID #
+        sekcijosPavadinimas.push(sectionID);                //surenka sekcijų pavadinimus į vieną sąrašą;
+    
+            if(scroll>=sections[i]){                        //jeigu skrolas yra tarp tikrinamos sekcijos ir tarp sekančios tikrinamos sekcijos 
+                sectionNameNow = ''+ sectionID;             // tada dabartinės sekcijos pavadinimas yra tos sek                         
+            }else{
+                continue;
+            }
+    }
+    console.log( sections );
+    console.log( scroll+'dabartinis aukštis' );
+    console.log( top );
+    console.log (sekcijuSarasas);
+    console.log(sectionID = sectionID.substring(1))
+    console.log( sekcijosPavadinimas );
+    console.log( 'sectionNameNow '+ sectionNameNow );
+}
+
+
+
+
+
+
+
+
+
+//HERO
 function generateIcons( data ) {
     var HTML = '';
     
@@ -12,6 +70,7 @@ function generateIcons( data ) {
 
     return HTML;
 }
+//BOTTOM
 function generateFooterIcons( data ) {
     var HTML = '';
     
