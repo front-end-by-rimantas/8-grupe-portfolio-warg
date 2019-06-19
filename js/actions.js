f351b8a399a83f817acb484aa71cee7748062a7e
Grumstukas <tartenaite.toma@gmail.com>
2019-06-19 15:58:46 +0300

    Education + experience

--- js/actions.js
+++ js/actions.js
@@ -2,14 +2,33 @@
 // general
 window.addEventListener("scroll", headerScrollDetector);
 
-//HERO
+// HERO section
 document.getElementById('icons').innerHTML = generateIcons( Icons );
 
-//BOTTOM
-document.getElementById('footer_icons').innerHTML = generateFooterIcons( footerIcons );
+// ABOUT ME section
 
-//SERVICES
+// MY SERVICES section
 document.getElementById('myServices').innerHTML = generateServices( servicesInfo );
 
-//STATISTICS
-document.getElementById('myStatistics').innerHTML = generateStatistics( statisticsInfo );
+// EDUCATION section
+document.getElementById('myEducation').innerHTML = generateEducation( educationInfo );
+// SKILLS section
+
+// EXPERIENCE section
+document.getElementById('myExperience').innerHTML = generateExperience( experienceInfo );
+// STATISTICS section
+
+// FREELANCE section
+
+// MY PORTFOLIO section
+
+// TESTIMONIALS section
+
+// MY BLOGS section
+
+// CONTACT ME section
+
+// BOTTOM NAVIGATION section
+document.getElementById('footer_icons').innerHTML = generateFooterIcons( footerIcons );
+
+// FOOTER section
