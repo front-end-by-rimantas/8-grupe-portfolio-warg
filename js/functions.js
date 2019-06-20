ceab964e1b796058f7d7d8b9165f1d4161ccd357
Grumstukas <tartenaite.toma@gmail.com>
2019-06-20 11:00:10 +0300

    correct path to photos

--- js/functions.js
+++ js/functions.js
@@ -179,12 +179,12 @@ function generateBlog ( data ) {
     var HTML = '';
     for (var i=0; i<data.length; i++) {
         HTML += `<div class="blog">
-        <div class="img" style="background-image: url(../img/blogs/${data[i].photo});"></div>
+        <div class="img" style="background-image: url(./img/blogs/${data[i].photo});"></div>
         <div class="blog-meta">
         <span class="color-text">${data[i].info[0]}</span><span class="usual-text"> ${data[i].info[1]}</span>
         <h3>${data[i].heading}</h3>
         <p>${data[i].description}</p>
-        <div class="img-2" style="background-image: url(../img/blogs/${data[i].personal_info[0]});"></div> 
+        <div class="img-2" style="background-image: url(./img/blogs/${data[i].personal_info[0]});"></div> 
         <span class="usual-text">${data[i].personal_info[1]}</span><span class="color-text"> ${data[i].personal_info[2]}</span>
         <div class="social-layer"><i class="fa fa-share-alt"></i></div>
         </div>
